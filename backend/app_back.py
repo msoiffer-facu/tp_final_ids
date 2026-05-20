87b74336232d0d83ed6dd3ba51863ca55b235e2d
Tiziano Laffargue <tizilaffargue@gmail.com>
2026-05-19 23:54:08 -0300

    subo creacion de tablas sql y compose

--- backend/app_back.py
+++ backend/app_back.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from tp_final_ids.backend.routes.login import *
+
+from backend.routes.alumnos import *
+from backend.routes.asistencia import *
+from backend.routes.equipos import *
+from backend.routes.login import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hola Flask"
+app.register_blueprint(asistencia_bp, url_prefix="/asistencia")
+app.register_blueprint(equipos_bp, url_prefix="/equipos")
+app.register_blueprint(login_bp, url_prefix="/login")
+app.register_blueprint(alumnos_bp, url_prefix="/alumnos")
 
 if __name__ == "__main__":
     app.run(debug=True)
